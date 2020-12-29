130c81817e1a262fd504aedb661e52dfd9d371de
abdo ghazi <abdogazy14@gmail.com>
2020-12-29 19:16:58 +0200

    "ecommerce"

--- app/src/main/java/com/example/e_commerce/di/FireBaseModule.kt
+++ app/src/main/java/com/example/e_commerce/di/FireBaseModule.kt
@@ -2,11 +2,13 @@ package com.example.e_commerce.di
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import javax.inject.Singleton
+
 
 @Module
 @InstallIn(ApplicationComponent::class)
@@ -14,7 +16,11 @@ class FireBaseModule {
     @Provides
     @Singleton
     fun provideFireStoreInstance(): FirebaseFirestore {
-        return FirebaseFirestore.getInstance()
+        val db = FirebaseFirestore.getInstance()
+        db.firestoreSettings = FirebaseFirestoreSettings.Builder()
+            .setPersistenceEnabled(true)
+            .build()
+        return db
     }
 
     @Provides
